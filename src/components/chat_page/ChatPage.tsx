b1d0e286c8e72bd0ea7ee55b9c6b5a80a31d676c
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-02-22 21:46:06 +0100

    Change the file

--- src/components/chat_page/ChatPage.tsx
+++ src/components/chat_page/ChatPage.tsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react'
-import style from './ChatPage.module.scss'
-import {Room} from '../../enums';
-import { io, Socket } from 'socket.io-client';
-import ChatController from './chat_controller/ChatController'
-import { AuthUser, UserConnection } from '../../models'
-import { getRandomUid } from '../../utils/random_generators'
-import ChatArea from './chat_area/ChatArea';
+import React, { useState, useEffect } from "react";
+import style from "./ChatPage.module.scss";
+import { Room } from "../../enums";
+import { io, Socket } from "socket.io-client";
+import ChatController from "./chat_controller/ChatController";
+import { AuthUser, UserConnection } from "../../models";
+import { getRandomUid } from "../../utils/random_generators";
+import ChatArea from "./chat_area/ChatArea";
 
-export default function ChatPage(props: {
-    room: Room;
-    nickname: string;
-}) {
+export default function ChatPage(props: { room: Room; nickname: string }) {
+  useEffect(() => {
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
+      userConnection.socket.close();
+    });
+  }, []);
 
-    const [userConnection, _] = useState(():UserConnection=> {
-        const authUser: AuthUser = {
-            room: (props.room + 1) || 1,
-            user: {
-                uid: getRandomUid(props.nickname || '__gio__'),
-                enteredOn: Date.now(),
-                nickname: props.nickname || '__gio__'
-            }
-        }
-        const socket = io('ws://localhost:3000', { 
-            auth: authUser,
-            hostname: 'localhost',
-        });
-        socket.on('message', (msg)=> console.log(msg))
-        return {
-            socket: socket,
-            user: authUser.user
-        }
-    })
+  const [userConnection, _] = useState(
+    (): UserConnection => {
+      const authUser: AuthUser = {
+        room: props.room + 1 || 1,
+        user: {
+          enteredOn: Date.now(),
+          nickname: props.nickname || "__gio__",
+        },
+      };
+      const socket = io("ws://localhost:3000", {
+        auth: authUser,
+        hostname: "localhost",
+      });
+      authUser.user.uid = socket.id;
 
-    return (
-        <div className={style.ChatPage}>
-            <h1>Welcome to the Room-{props.room + 1} </h1>
-            <ChatArea userConnection={userConnection}/>
-            <ChatController/>
-        </div>
-    )
+      return {
+        socket: socket,
+        user: authUser.user,
+      };
+    }
+  );
+  console.log(userConnection);
+  return (
+    <div className={style.ChatPage}>
+      <h1 style={{ textAlign: "center" }}>
+        Welcome to the Room-{props.room + 1}
+      </h1>
+      <ChatArea userConnection={userConnection} />
+      <ChatController />
+    </div>
+  );
 }
