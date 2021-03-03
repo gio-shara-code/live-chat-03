37afdb9371c0886e888848c17fcbd33383226b9c
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-03-03 14:19:47 +0100

    added test input renders in PrimaryTextField

--- src/components/primary_text_field/PrimaryTextField.test.tsx
+++ src/components/primary_text_field/PrimaryTextField.test.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 import PrimaryTextField from "./PrimaryTextField";
 describe("PrimaryTextField", () => {
   const pErrorRoleName = "display-error";
+
+  it("input renders", () => {
+    const { getByRole } = render(<PrimaryTextField error="error" />);
+    expect(getByRole("textbox")).toHaveClass("PrimaryTextField", {
+      exact: true,
+    });
+  });
+
   it("does show error", () => {
     const { getByRole } = render(<PrimaryTextField error="error" />);
     expect(getByRole(pErrorRoleName)).toHaveTextContent(/^error$/);
@@ -15,5 +24,10 @@ describe("PrimaryTextField", () => {
     expect(getByRole(pErrorRoleName)).toHaveClass("Error ErrorInActive", {
       exact: true,
     });
+  });
+  it("onChange is working", () => {
+    const { getByRole } = render(<PrimaryTextField />);
+
+    expect(getByRole("textbox")); //default input role -> testbox (type=text)
   });
 });
