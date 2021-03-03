e3667b3e0e387ba2b9be0d8f95a8a0095fc59a94
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-03-03 10:08:03 +0100

    Added test for primary button

--- src/components/primary_button/PrimaryButton.test.tsx
+++ src/components/primary_button/PrimaryButton.test.tsx
@@ -4,16 +4,18 @@ import "@testing-library/jest-dom/extend-expect";
 import PrimaryButton from "./PrimaryButton";
 
 it("is not active and renders", () => {
+  //arrange
   const { getByRole } = render(
     <PrimaryButton active={false}>Click Me</PrimaryButton>
   );
+
+  //act & assert
   expect(getByRole("button")).toHaveTextContent("Click Me");
   expect(getByRole("button")).toHaveClass("PrimaryButton InActive");
 });
 
 it("is active and renders", () => {
   const { getByRole } = render(<PrimaryButton active>Click Me</PrimaryButton>);
-  const primaryButton = getByRole("button");
 
   expect(getByRole("button")).toHaveTextContent("Click Me");
   expect(getByRole("button")).toHaveClass("PrimaryButton Active");
