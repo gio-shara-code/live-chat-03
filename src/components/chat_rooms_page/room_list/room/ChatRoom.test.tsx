3327cd014d959ea171ec61c80c9bbe42052e0190
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-03-03 22:29:12 +0100

    Added onclick test on ChatRoom

--- src/components/chat_rooms_page/room_list/room/ChatRoom.test.tsx
+++ src/components/chat_rooms_page/room_list/room/ChatRoom.test.tsx
@@ -2,23 +2,32 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ChatRoom from "./ChatRoom";
+import userEvent from "@testing-library/user-event";
 describe("ChatRoom", () => {
+  const aLinkRole = "chat-room-a";
   it("renders children", () => {
     const { getByRole } = render(<ChatRoom>Hello</ChatRoom>);
-    expect(getByRole("chat-room-a")).toHaveTextContent("Hello");
+    expect(getByRole(aLinkRole)).toHaveTextContent("Hello");
   });
 
   it("renders component as active", () => {
     const { getByRole } = render(<ChatRoom isActive />);
-    expect(getByRole("chat-room-a")).toHaveClass("Room RoomActive", {
+    expect(getByRole(aLinkRole)).toHaveClass("Room RoomActive", {
       exact: true,
     });
   });
 
   it("renders component as inactive", () => {
     const { getByRole } = render(<ChatRoom isActive={false} />);
-    expect(getByRole("chat-room-a")).toHaveClass("Room", {
+    expect(getByRole(aLinkRole)).toHaveClass("Room", {
       exact: true,
     });
+  });
+
+  it("onClick is working", () => {
+    const handleClick = jest.fn();
+    const { getByRole } = render(<ChatRoom onClick={handleClick} />);
+    userEvent.click(getByRole(aLinkRole));
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
