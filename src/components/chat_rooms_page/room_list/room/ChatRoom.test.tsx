152e4fe74ec57cfee7670f609c5da6f26fec40d7
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-03-03 22:21:11 +0100

    Added first test

--- src/components/chat_rooms_page/room_list/room/ChatRoom.test.tsx
+++ src/components/chat_rooms_page/room_list/room/ChatRoom.test.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ChatRoom from "./ChatRoom";
+describe("ChatRoom", () => {
+  it("renders children", () => {
+    const { getByRole } = render(<ChatRoom>Hello</ChatRoom>);
+    expect(getByRole("chat-room-a")).toHaveTextContent("Hello");
+  });
 
-describe("ChatRoom", () => {});
+  it("Hello World", () => {});
+});
--- src/components/chat_rooms_page/room_list/room/ChatRoom.tsx
+++ src/components/chat_rooms_page/room_list/room/ChatRoom.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import style from "./ChatRoom.module.scss";
 
 export default function ChatRoom(props: {
-  onClick;
-  children: string;
-  isActive: boolean;
+  onClick?;
+  children?: string;
+  isActive?: boolean;
 }) {
   let roomStyle = [style.Room];
   if (props.isActive) roomStyle.push(style.RoomActive);
   return (
     <li>
-      <a href="#" onClick={props.onClick} className={roomStyle.join(" ")}>
+      <a
+        role="chat-room-a"
+        onClick={props.onClick}
+        className={roomStyle.join(" ")}
+      >
         {props.children}
       </a>
     </li>
